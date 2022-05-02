22b7524cebf247a24d86cee6c63aea1cb9d3e037
Aldukhov <vasia200257@gmail.com>
2022-05-02 13:07:18 +0300

    Button
    Button

--- Engine/Game/Tower state/Blacksmith.cpp
+++ Engine/Game/Tower state/Blacksmith.cpp
@@ -11,31 +11,47 @@ BlackSmith::BlackSmith(System& system, Hero* hero, bool *isLoadSource)
 	bEvents[0] = new Dialog(system, hero, isLoadSource);
 	bEvents[1] = new UpgradeWeapon(system, hero, isLoadSource);
 	bEvents[2] = new Farewell(system, hero, isLoadSource);
+	stateBlack = 0;
 }
 
 void BlackSmith::update()
 {
-}
+	try 
+	{
+		bEvents[stateBlack]->update();
+	}
+	catch (size_t id) 
+	{
+		bEvents[stateBlack]->removeSource();
+		stateBlack = id;
+		bEvents[stateBlack]->createSource();
+	}
+}	
 
 void BlackSmith::render()
 {
+	bEvents[stateBlack]->render();
+
 }
 
 void BlackSmith::draw()
 {
+	bEvents[stateBlack]->draw();
 }
 
 void BlackSmith::hud()
 {
-	/*..*/
+	return;
 }
 
 void BlackSmith::createSource()
 {
+	//фон, звук
 }
 
 void BlackSmith::removeSource()
 {
+	//фон, звук
 }
 
 BlackSmith::~BlackSmith()
